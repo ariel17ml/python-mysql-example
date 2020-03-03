26523cc00b0b2a6620caef966da41f2a66ed4390
Ariel Gerardo Ríos <ariel.rios@mercadolibre.com>
2020-03-03 10:00:41 -0300

    Adds Python connect example for MySQL.

---
+++ env.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+pip install mysql-connector-python
---
+++ example.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import mysql.connector
+
+db_connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root",
+    database="mysql",
+    auth_plugin='mysql_native_password'
+)
+
+db_cursor = db_connection.cursor()
+db_cursor.execute("CREATE TABLE employee(\
+                  id INT AUTO_INCREMENT PRIMARY KEY, \
+                  name VARCHAR(255), salary INT(6))")
+db_cursor.execute("SHOW TABLES")
+for table in db_cursor:
+    print(table)
